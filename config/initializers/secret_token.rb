e4a54a17ba4b372b966ca1ef0a51b2576d84171f
Gryphis <gryphis@gmx.de>
2012-12-28 20:50:50 +0100

    rails new inventar -d mysql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventar::Application.config.secret_token = 'bee7b223ec6abab883cbc18b7ebfbe7d39bcd5bfbc27bed95a1f6a83de47e93a5df38909fa12926724eeb1d16e58bcde123324ac3ef69bc9be6e0ef231ec9f46'
