e08b461d20bb6ab2749ff7c464211c7b55acd261
Gryphis <gryphis@gmx.de>
2012-12-28 21:34:58 +0100

    rails => 3.2.9

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Inventar::Application.config.secret_token = 'bee7b223ec6abab883cbc18b7ebfbe7d39bcd5bfbc27bed95a1f6a83de47e93a5df38909fa12926724eeb1d16e58bcde123324ac3ef69bc9be6e0ef231ec9f46'
+Inventar::Application.config.secret_token = '07fdf56bbdda2fb7add4bb3d8b2ea85f190b9a8829022a9bb9fff9cffb1759cf33df4828ba88f6fb4d463f304df4777e97cd2195410911858df8b84ab357348c'
